5054bf8be60c47e9005cb4006f6d0b90919fa17b
Vitória Monteiro <monteirovitoria29@gmail.com>
2025-06-23 15:29:02 -0300

    Criação do menu hamburguer

---
+++ assets/js/menu.js
@@ -0,0 +1,7 @@
+document.getElementById("menu-toggle").addEventListener("click", function () {
+  document.getElementById("nav-menu").classList.add("show");
+});
+
+document.getElementById("close-menu").addEventListener("click", function () {
+  document.getElementById("nav-menu").classList.remove("show");
+});
