980f5c2601cb78be36565c5c6e8421c0347ab895
Vitória Monteiro <monteirovitoria29@gmail.com>
2025-06-23 23:02:28 -0300

    criação da pagina Doe Agora. Ps: Pagina já responsiva

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,50 +1,10 @@
-function carregarNavEFooter() {
-  const navHtml = `
-    <header>
-      <img class="logo" src="assets/img/logo.png" alt="Logo Moral de Cria">
-      <nav>
-        <a href="index.html">HOME</a>
-        <a href="sobrenos.html">SOBRE NÓS</a>
-        <a href="projetos.html">PROJETOS</a>
-        <a href="participe.html">PARTICIPE</a>
-        <a href="contato.html">CONTATO</a>
-        <a href="https://" target="_blank" rel="noopener noreferrer" id="btn-header">DOE AGORA</a>
-      </nav>
-    </header>
-  `;
-
-  const footerHtml = `
-    <footer>
-      <section class="footer-logo">
-        <img src="assets/img/logo white.png" alt="Logo Moral de Cria">
-        <p>Somos motivados a fazer de tudo para tornar o mundo um lugar melhor</p>
-      </section>
-      <section class="redes-sociais">
-        <div class="nav-bar">
-          <a href="index.html">HOME</a>
-          <a href="sobrenos.html">SOBRE NÓS</a>
-          <a href="projetos.html">PROJETOS</a>
-          <a href="participe.html">PARTICIPE</a>
-          <a href="contato.html">CONTATO</a>
-        </div>
-        <div class="redes">
-          <p>Nos acompanhe</p>
-          <div class="redes-container">
-            <a href="https://www.instagram.com/moraldecria2" target="_blank" rel="noopener"><i class="fa-brands fa-instagram"></i></a>
-            <a href="https://www.instagram.com/casa_deformacaoprofissionalmdc" target="_blank" rel="noopener"><i class="fa-brands fa-instagram"></i></a>
-            <a href="https://youtube.com/@ongmoraldecria" target="_blank" rel="noopener"><i class="fa-brands fa-youtube"></i></a>
-            <a href="https://www.facebook.com/coletivomoraldecria" target="_blank" rel="noopener"><i class="fa-brands fa-facebook-f"></i></a>
-          </div>
-        </div>
-      </section>
-      <section class="direitos">
-        <p>© 2024 ONG Moral de Cria - CNPJ: 54.610.631/0001-51 | Todos os direitos reservados</p>
-      </section>
-    </footer>
-  `;
-
-  document.getElementById('nav-container').innerHTML = navHtml;
-  document.getElementById('footer-container').innerHTML = footerHtml;
+function copiarPix() {
+  const chave = document.querySelector(".chave").innerText;
+  navigator.clipboard.writeText(chave)
+    .then(() => {
+      alert("Chave PIX copiada com sucesso!");
+    })
+    .catch(() => {
+      alert("Erro ao copiar a chave PIX. Tente manualmente.");
+    });
 }
-
-window.onload = carregarNavEFooter;
